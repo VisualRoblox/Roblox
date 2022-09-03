339fb851b913aca3abb1ab79b69311881c93d7c9
CallumRamsey <mappletube@gmail.com>
2022-09-04 00:08:37 +0100

    Update DestroyUI.lua

--- Utilities/Visual/UI/Functions/DestroyUI.lua
+++ Utilities/Visual/UI/Functions/DestroyUI.lua
@@ -13,10 +13,16 @@ function DestroyUIHandler.DestroyUI()
     if CoreGui:FindFirstChild('VisualAnalyser') then
         local UI = CoreGui:FindFirstChild('VisualAnalyser')
 
+        for _, Item in next, UI.Base:GetChildren() do
+            if Item.Name ~= 'BaseCorner' and Item.Name ~= 'BaseStroke' then
+                Item:Destroy()
+            end
+        end
+
         Tween(UI.Base, {Size = UDim2.new(0, 0, 0, 0)}, 0.25)
         task.wait(0.25)
 
-        UI.Base:Destroy()
+        UI:Destroy()
         LoggingHandler.Log('UI Destroyed')
 
         Visual.Loaded = false 
