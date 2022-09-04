a4e710f3dfa051622652b57bceea7f41c6712ef4
CallumRamsey <mappletube@gmail.com>
2022-09-04 19:34:17 +0100

    Update ToggleUI.lua

--- Utilities/Visual/UI/Functions/ToggleUI.lua
+++ Utilities/Visual/UI/Functions/ToggleUI.lua
@@ -17,6 +17,7 @@ function ToggleUIHandler.ToggleUI()
         Tween(OpenButton, {ImageTransparency = 0}, 0.25)
         OpenButton.OpenButtonBaseStroke.Thickness = 1
         Tween(Base, {Size = UDim2.new(0, 0, 0, 0)}, 0.25)
+        Base.Visible = false
     else
         local Base = CoreGui:FindFirstChild('VisualAnalyser').Base
         local OpenButton = CoreGui:FindFirstChild('VisualAnalyser').OpenButton
@@ -24,6 +25,7 @@ function ToggleUIHandler.ToggleUI()
         Tween(OpenButton, {ImageTransparency = 1}, 0.25)
         OpenButton.OpenButtonBaseStroke.Thickness = 0
         Tween(Base, {Size = UDim2.new(0, 650, 0, 375)}, 0.25)
+        Base.Visible = true
     end
 end
 
