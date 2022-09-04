2626ca64e93967ba3dbe5e411f3314c6c2bbc675
CallumRamsey <mappletube@gmail.com>
2022-09-04 01:07:37 +0100

    Update Dragging.lua

--- Utilities/Visual/UI/Functions/Dragging.lua
+++ Utilities/Visual/UI/Functions/Dragging.lua
@@ -1,16 +1,22 @@
+-- // Services
+local UserInputService = game:GetService('UserInputService')
+
 local DraggingHandler = {}
 
 function DraggingHandler.EnableDragging(Frame)
-    local UserInputService = game:GetService('UserInputService')
-
-    local Dragging, DraggingInput, MousePosition, FramePosition
-
+    local Dragging, DraggingInput, DragStart, StartPosition
+        
+    local function Update(Input)
+        local Delta = Input.Position - DragStart
+        Frame.Position = UDim2.new(StartPosition.X.Scale, StartPosition.X.Offset + Delta.X, StartPosition.Y.Scale, StartPosition.Y.Offset + Delta.Y)
+    end
+    
     Frame.InputBegan:Connect(function(Input)
         if Input.UserInputType == Enum.UserInputType.MouseButton1 then
             Dragging = true
-            MousePosition = Input.Position
-            FramePosition = Frame.Parent.Position
-            
+            DragStart = Input.Position
+            StartPosition = Frame.Position
+    
             Input.Changed:Connect(function()
                 if Input.UserInputState == Enum.UserInputState.End then
                     Dragging = false
@@ -18,17 +24,16 @@ function DraggingHandler.EnableDragging(Frame)
             end)
         end
     end)
-
+    
     Frame.InputChanged:Connect(function(Input)
         if Input.UserInputType == Enum.UserInputType.MouseMovement then
             DraggingInput = Input
         end
     end)
-
+    
     UserInputService.InputChanged:Connect(function(Input)
         if Input == DraggingInput and Dragging then
-            local Delta = Input.Position - MousePosition
-            Frame.Parent.Position  = UDim2.new(FramePosition.X.Scale, FramePosition.X.Offset + Delta.X, FramePosition.Y.Scale, FramePosition.Y.Offset + Delta.Y)
+            Update(Input)
         end
     end)
 end
