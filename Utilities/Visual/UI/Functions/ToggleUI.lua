8f0228ecbbf7e7e837a1379dc413f07ea3bc3912
CallumRamsey <mappletube@gmail.com>
2022-09-04 17:42:48 +0100

    Create ToggleUI.lua

---
+++ Utilities/Visual/UI/Functions/ToggleUI.lua
@@ -0,0 +1,29 @@
+-- // Services
+local CoreGui = game:GetService('CoreGui')
+
+-- // Imports
+local Tween = Import('UI/Functions/Tween.lua')
+
+local ToggleUIHandler = {}
+
+function ToggleUIHandler.ToggleUI()
+    LoggingHandler.Log('Toggle UI Called')
+
+    if CoreGui:FindFirstChild('VisualAnalyser').Base.Visible then
+        local Base = CoreGui:FindFirstChild('VisualAnalyser').Base
+        local OpenButton = CoreGui:FindFirstChild('VisualAnalyser').OpenButton
+        Tween(OpenButton, {BackgroundTransparency = 0}, 0.25)
+        Tween(OpenButton, {ImageTransparency = 0}, 0.25)
+        OpenButton.OpenButtonBaseStroke.Thickness = 1
+        Tween(Base, {Size = UDim2.new(0, 0, 0, 0)}, 0.25)
+    else
+        local Base = CoreGui:FindFirstChild('VisualAnalyser').Base
+        local OpenButton = CoreGui:FindFirstChild('VisualAnalyser').OpenButton
+        Tween(OpenButton, {BackgroundTransparency = 1}, 0.25)
+        Tween(OpenButton, {ImageTransparency = 1}, 0.25)
+        OpenButton.OpenButtonBaseStroke.Thickness = 0
+        Tween(Base, {Size = UDim2.new(0, 650, 0, 375)}, 0.25)
+    end
+end
+
+return ToggleUIHandler.ToggleUI()
