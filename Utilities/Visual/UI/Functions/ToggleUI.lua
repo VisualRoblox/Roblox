5ca5e55a5f2d279da1d1b98816d58a4dceea3dac
CallumRamsey <mappletube@gmail.com>
2022-09-04 18:34:13 +0100

    Update ToggleUI.lua

--- Utilities/Visual/UI/Functions/ToggleUI.lua
+++ Utilities/Visual/UI/Functions/ToggleUI.lua
@@ -7,16 +7,16 @@ local Tween = Import('UI/Functions/Tween.lua')
 local ToggleUIHandler = {}
 
 function ToggleUIHandler.ToggleUI()
-    if CoreGui:FindFirstChild('VisualAnalyser'):FindFirstChild('Base').Visible then
-        local Base = CoreGui:FindFirstChild('VisualAnalyser').Base
-        local OpenButton = CoreGui:FindFirstChild('VisualAnalyser').OpenButton
+    if CoreGui:WaitForChild('VisualAnalyser'):WaitForChild('Base').Visible then
+        local Base = CoreGui:WaitForChild('VisualAnalyser').Base
+        local OpenButton = CoreGui:WaitForChild('VisualAnalyser').OpenButton
         Tween(OpenButton, {BackgroundTransparency = 0}, 0.25)
         Tween(OpenButton, {ImageTransparency = 0}, 0.25)
         OpenButton.OpenButtonBaseStroke.Thickness = 1
         Tween(Base, {Size = UDim2.new(0, 0, 0, 0)}, 0.25)
     else
-        local Base = CoreGui:FindFirstChild('VisualAnalyser').Base
-        local OpenButton = CoreGui:FindFirstChild('VisualAnalyser').OpenButton
+        local Base = CoreGui:WaitForChild('VisualAnalyser').Base
+        local OpenButton = CoreGui:WaitForChild('VisualAnalyser').OpenButton
         Tween(OpenButton, {BackgroundTransparency = 1}, 0.25)
         Tween(OpenButton, {ImageTransparency = 1}, 0.25)
         OpenButton.OpenButtonBaseStroke.Thickness = 0
