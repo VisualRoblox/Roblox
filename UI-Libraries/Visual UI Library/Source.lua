d0d84c24293f32ba79468272e902495bfaa0af01
Mapple <84987152+Mapple7777@users.noreply.github.com>
2022-09-08 18:08:56 +0100

    Update Source.lua

--- UI-Libraries/Visual UI Library/Source.lua
+++ UI-Libraries/Visual UI Library/Source.lua
@@ -1170,7 +1170,7 @@ function Library:CreateWindow(HubName, GameName, IntroText, IntroIcon, ImprovePe
                 BackgroundTransparency = 1,
                 TextTransparency = 1,
                 AnchorPoint = Vector2.new(0.5, 0.5),
-                Position = UDim2.new(0.5, 0, 0.5, -25),
+                Position = UDim2.new(0.5, 0, 0.5, -40),
                 BorderSizePixel = 0,
                 Size = UDim2.new(0, 170, 0, 20),
                 Font = Enum.Font.Gotham,
@@ -1191,7 +1191,7 @@ function Library:CreateWindow(HubName, GameName, IntroText, IntroIcon, ImprovePe
                 ZIndex = 3,
                 Size = UDim2.new(0, 100, 0, 100),
                 Image = IntroIcon,
-                ScaleType = 'Fit'
+                ScaleType = Enum.ScaleType.Fit
             })
         }),
     })
@@ -1208,7 +1208,7 @@ function Library:CreateWindow(HubName, GameName, IntroText, IntroIcon, ImprovePe
     Utility:Tween(Main['IntroText'], {TextTransparency = 0}, 0.25)
     task.wait(0.5)
     Utility:Tween(Main['IntroImage'], {ImageTransparency = 0}, 0.25)
-    task.wait(3)
+    task.wait(99)
     Utility:Tween(Main['IntroText'], {TextTransparency = 1}, 0.25)
     task.wait(0.5)
     Utility:Tween(Main['IntroImage'], {ImageTransparency = 1}, 0.25)
