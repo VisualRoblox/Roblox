ab67490ebe2c8e43d4d05ffb9dc9bed8c34def59
CallumRamsey <mappletube@gmail.com>
2022-09-11 23:09:06 +0100

    x

---
+++ UI-Libraries/Visual Command Library/README.md
@@ -0,0 +1,1 @@
+# Visual Command Library
---
+++ UI-Libraries/Visual Command Library/Source.lua
@@ -0,0 +1,9 @@
+-- // Services
+local CoreGui = game:GetService('CoreGui')
+local TweenService = game:GetService('TweenService')
+local UserInputService = game:GetService('UserInputService')
+local TextService = game:GetService('TextService')
+
+-- // Variables
+local Library = {}
+local UtilityFunctions
